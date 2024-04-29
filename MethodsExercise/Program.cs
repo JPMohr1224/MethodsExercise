5349f26e0fcc1dc9fd1af3149f919e928f795880
JP Mohr <James.Mohr2.0@hotmail.com>
2024-04-29 12:46:31 -0400

    added methods to improve functionality

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,42 +1,40 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
-            
+            string name = "";
+            string animal = "";
+            string color = "";
+            string band = "";
 
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Thanks {name}! Lets just go over a few more questions.");
-            Console.WriteLine("Whats your favorite color?");
-            string color = Console.ReadLine();
-            Console.WriteLine("Whats your favorite animal?");
-            string animal = Console.ReadLine();
-            Console.WriteLine("Whats your favorite band?");
-            string band = Console.ReadLine();
+            Greeting();
+            FillInTheColor();
+            FillInTheAnimal();
+            FillInTheBand();
+
             Console.WriteLine("Thanks for answerinmg my questions. Here's a little story we just made together,");
             Console.WriteLine($"Once upon a time in the enchanted substack, {name} the {animal} was checking his code for errors.");
             Console.WriteLine($"90% of their day was spent looking for errors. That made them feel {color}.");
             Console.WriteLine($"{name} decided to listen to some music to feel better. They played some {band} because that was their favoirite!");
-            
-            string str1;
-            string str2;
-
-            int num1;
-            int num2;
 
 
-            Console.WriteLine("Now lets try some math functions!");
-            Console.WriteLine("First I need you tyo choose two numbers");
-            Console.WriteLine("Please enter your first number.");
-            str1 = Console.ReadLine();
-            Console.WriteLine("Please enter your second number.");
-            str2 = Console.ReadLine();
+
+            string str1 = "";
+            string str2 = "";
+
+            int num1 = 0;
+            int num2 = 0;
             num1 = int.Parse(str1);
             num2 = int.Parse(str2);
 
+            MathSetUp1();
+            MathSetUp2();
+           
 
             int solveSum = num1 + num2;
             int solveSub = num1 - num2;
@@ -51,18 +49,90 @@
             Console.WriteLine($"Division: {num1} / {num2} equals {solveDivide}.\n");
             Console.WriteLine($"Modulus: {num1} % {num2} equals {solveMod}.\n");
 
-            string howManyTimes;
+            string HowManyT = "";
+            HowManyTimes();
+           
+        }
+
+
+    //Exercise One Methods
+
+
+        public static string Greeting()
+        {
+            Console.WriteLine("What's your name?");
+           string  name = Console.ReadLine();
+            Console.WriteLine($"Thanks {name}! Lets just go over a few more questions.");
+            return name;
+        }
+
+        public static string FillInTheAnimal()
+        {
+            Console.WriteLine("Whats your favorite animal?");
+            string animal = Console.ReadLine();
+            return animal;
+        }
+
+        public static string FillInTheColor()
+        {
+            Console.WriteLine("Whats your favorite color?");
+            string color = Console.ReadLine();
+            return color;
+        }
+
+        public static string FillInTheBand()
+        {
+            Console.WriteLine("Whats your favorite band?");
+            string band = Console.ReadLine();
+            return band;
+        }
+
+
+
+        
+     //Exercise Two Methods      
+        
+
+
+            
+
+        public static string MathSetUp1()
+        {
+            Console.WriteLine("Now lets try some math functions!");
+            Console.WriteLine("First I need you tyo choose two numbers");
+            Console.WriteLine("Please enter your first number.");
+            string str1 = Console.ReadLine();
+            return str1;
+            
+        }
+
+        public static string MathSetUp2()
+        {
+            Console.WriteLine("Please enter your second number.");
+            string str2 = Console.ReadLine();
+            return str2;
+        }
+
+    
+
+    // Bonus Methods        
+
+        public static string HowManyTimes() {
             int i = 0;
             int sum1;
             int totalSum = 0;
-
             Console.WriteLine("***BONUS***BONUS***BONUS***");
             Console.WriteLine("pick a number");
-            howManyTimes = Console.ReadLine();
-            int howManyInt = int.Parse(howManyTimes);
-            Console.WriteLine($"You picked {howManyTimes}, so now you can pick that many numbers and we can see the total.");
+            HowManyT = Console.ReadLine();
+           
 
-            while  (i < howManyInt)
+            int HowManyInt = int.Parse(HowManyTimes);
+            Console.WriteLine($"You picked {HowManyTimes}, so now you can pick that many numbers and we can see the total.");
+
+
+
+
+            while (i < HowManyInt)
             {
                 Console.WriteLine("Pick a number\n");
     
@@ -73,6 +143,6 @@
             Console.WriteLine($"The total of the numbers that you entered is {totalSum}.");
         }
 
-
+        
     }
 }
